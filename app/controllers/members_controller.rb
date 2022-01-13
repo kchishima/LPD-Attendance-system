1bf1d4735c6d9286fc8745c83db6f4ab7731a607
kchishima <keito.kubomachi@gmail.com>
2022-01-13 01:05:32 +0000

    add_rogin

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,7 @@
+class MembersController < ApplicationController
+  def new
+  end
+  def show
+     @member = Member.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :members
+  root to: 'members#new'
+  root to: "home#index"
+  resources :members, only: [:show]
+
 end
